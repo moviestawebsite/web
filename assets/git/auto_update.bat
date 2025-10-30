fb32d5a6c490b08e1c67e5e034ad6d5b23660c99
Learnovo <learnovo.platform@gmail.com>
2025-10-30 21:24:44 +0300

    Auto update: Thu 10/30/2025 21:24:44.13

---
+++ assets/git/auto_update.bat
@@ -0,0 +1,8 @@
+@echo off
+cd /d "D:\Documents\My Programming Projects\Html\Movie"
+:loop
+git add .
+git commit -m "Auto update: %date% %time%" 2>nul
+git push origin main 2>nul
+timeout /t 10 >nul
+goto loop
