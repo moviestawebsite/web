cdd53f43a4bab164ea330f3b1f93ea606c828dbc
Learnovo <learnovo.platform@gmail.com>
2025-11-01 22:24:11 +0200

    Auto update: Sat 11/01/2025 22:24:11.29

--- assets/js/watch-now.js
+++ assets/js/watch-now.js
@@ -164,13 +164,6 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 function renderMediaCard(item) {
   return `
-    <iframe 
-  width="100%" 
-  height="400" 
-  src="https://www.youtube.com/embed/live_stream?channel=UCabcd12345xyz" 
-  frameborder="0" 
-  allowfullscreen>
-</iframe>
     <div class="media-card" data-id="${item.id}">
         <img src="${item.image}" alt="${item.title}" class="media-thumb" />
     </div>
