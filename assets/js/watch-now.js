78c36d7d38add5b86112984a13adaf0c1285c631
Learnovo <learnovo.platform@gmail.com>
2025-11-01 22:37:20 +0200

    Auto update: Sat 11/01/2025 22:37:20.25

--- assets/js/watch-now.js
+++ assets/js/watch-now.js
@@ -140,7 +140,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     window.mediaData = data.media;
 
     container.innerHTML = `
-                  <iframe width="100%" height="400" src="https://www.youtube.com/embed/live_stream?channel=UCHxZfWDxxumOyTN0nvbRM5A"
+                  <iframe width="1200" height="400" src="https://www.youtube.com/embed/live_stream?channel=UCHxZfWDxxumOyTN0nvbRM5A"
                 frameborder="0" allowfullscreen>
             </iframe>
       <div class="media-grid">
