07780c03beec944602e5942185930be97222fb2e
Learnovo <learnovo.platform@gmail.com>
2025-11-03 21:59:18 +0200

    Auto update: Mon 11/03/2025 21:59:18.28

--- assets/js/indictor.js
+++ assets/js/indictor.js
@@ -1,15 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const liveBadge = document.getElementById("liveBadge");
-
     if (!liveBadge) return;
 
-    // تحقق من وجود المتغير
-    if (typeof window.isLive !== "undefined") {
-        liveBadge.style.display = window.isLive ? "inline-block" : "none";
+    // استخدم القيمة من watch-now.js
+    if (typeof window.isLiveNow !== "undefined") {
+        liveBadge.style.display = window.isLiveNow ? "inline-block" : "none";
     }
-
-    // لو المتغير ممكن يتغير بعد التحميل، تقدر تراقبه مثلاً بهذا الشكل:
-    // setInterval(() => {
-    //     liveBadge.style.display = window.isLive ? "inline-block" : "none";
-    // }, 500); // يتابع كل نصف ثانية
 });
