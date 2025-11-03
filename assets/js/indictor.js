d0c5c1e3f60889a6b1f05e842c54f6aec3b6b490
Learnovo <learnovo.platform@gmail.com>
2025-11-03 22:00:54 +0200

    Auto update: Mon 11/03/2025 22:00:54.33

--- assets/js/indictor.js
+++ assets/js/indictor.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const liveBadge = document.getElementById("liveBadge");
     if (!liveBadge) return;
 
-    // استخدم القيمة من watch-now.js
     if (typeof window.isLiveNow !== "undefined") {
         liveBadge.style.display = window.isLiveNow ? "inline-block" : "none";
     }
