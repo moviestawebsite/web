65112bd16a8a60fe2f347ad73a6ac3aa2944a117
Learnovo <learnovo.platform@gmail.com>
2025-11-03 21:50:42 +0200

    Auto update: Mon 11/03/2025 21:50:42.30

--- assets/js/indictor.js
+++ assets/js/indictor.js
@@ -1,9 +1,7 @@
 // live-point.js
 document.addEventListener("DOMContentLoaded", () => {
   const liveBadge = document.getElementById("liveBadge");
-  if (!liveBadge) return;
-
-  // تأكد من وجود قيمة عالمية
-  const isLive = window.isLiveNow ?? false;
-  liveBadge.style.display = isLive ? "inline-block" : "none";
+  if (liveBadge) {
+    liveBadge.style.display = window.isLiveNow ? "inline-block" : "none";
+  }
 });
