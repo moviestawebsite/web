9a37d9a0d0207fc6a075e9b4519f32711a9e68f3
Learnovo <learnovo.platform@gmail.com>
2025-11-03 20:48:55 +0200

    Auto update: Mon 11/03/2025 20:48:55.36

--- assets/js/watch-now.js
+++ assets/js/watch-now.js
@@ -176,7 +176,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         <div class="video-wrapper" style="position:relative;">
           <video id="liveVideo" src="${url}" autoplay muted loop controls></video>
           <div class="live-indicator">
-            <span class="red-dot"></span>
+            <span class="live-badge"></span>
             <p>Live</p>
           </div>
         </div>
