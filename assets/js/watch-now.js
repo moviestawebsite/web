cdbd29dc81c6680d1db0c159c762db24ea5a000c
Learnovo <learnovo.platform@gmail.com>
2025-11-03 18:41:39 +0200

    Auto update: Mon 11/03/2025 18:41:39.30

--- assets/js/watch-now.js
+++ assets/js/watch-now.js
@@ -249,7 +249,6 @@ function renderMediaCard(item) {
   `;
 }
 
-
 // ======================= كود الـ Popup =======================
 function openPopup(item) {
   const loader1 = document.getElementById("popupLoader");
