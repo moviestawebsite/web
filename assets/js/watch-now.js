4199a776c333ddaf7eeaf80330eaf999852b6935
Learnovo <learnovo.platform@gmail.com>
2025-11-03 21:39:29 +0200

    Auto update: Mon 11/03/2025 21:39:29.28

--- assets/js/watch-now.js
+++ assets/js/watch-now.js
@@ -181,10 +181,10 @@ document.addEventListener("DOMContentLoaded", async () => {
         </div>
       </div>
       `;
+      const video_player = liveContainer.querySelector(".video_player");
+      if (video_player) initializeAllVideoPlayersFor(video_player);
     }
 
-    const video_player = liveContainer.querySelector(".video_player");
-    if (video_player) initializeAllVideoPlayersFor(video_player);
 
     if (videoItem.isLive) {
       liveBadge.style.display = "inline-block"; // تظهر
