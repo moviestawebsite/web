c4acf92c4e92767c5db039513e39a041b105a46d
Learnovo <learnovo.platform@gmail.com>
2025-11-05 21:08:33 +0200

    Auto update: Wed 11/05/2025 21:08:33.23

--- assets/js/indictor.js
+++ assets/js/indictor.js
@@ -1,6 +1,10 @@
 // live-status.js
 document.addEventListener("DOMContentLoaded", async () => {
   try {
+    const response = await fetch("../data/json/videos-database.json");
+    const data = await response.json();
+
+    // افترض إن فيه مفتاح اسمه isLive فى JSON
     const isLive = data.isLive;
 
     localStorage.setItem("liveStatus", isLive ? "true" : "false");
